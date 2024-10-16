1939ff113aa5bb14e3e300a011711ea9b7925041
winning godspower <winninggodspower@gmail.com>
2024-10-16 16:46:07 +0100

    feat: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: netlify(),
   integrations: [
     tailwind({
     // applyBaseStyles: false,
